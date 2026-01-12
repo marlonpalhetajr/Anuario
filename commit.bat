0189ca4bece4d162f71d5079a8d7c888b413434e
Marlon <junior01019@gmail.com>
2026-01-12 11:09:34 -0300

    Atualizar download dos mapas

---
+++ commit.bat
@@ -0,0 +1,6 @@
+@echo off
+cd "c:\Users\marlon.junior\OneDrive - Fapespa\anuario2024"
+"C:\Program Files\Git\bin\git.exe" add mapas.html
+"C:\Program Files\Git\bin\git.exe" commit -m "Adicionar nomes dos mapas embaixo de cada card"
+"C:\Program Files\Git\bin\git.exe" push
+pause
---
+++ commit.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd "/c/Users/marlon.junior/OneDrive - Fapespa/anuario2024"
+git add mapas.html
+git commit -m "Adicionar nomes dos mapas embaixo de cada card"
+git push
